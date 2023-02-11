7bf83aaded1c78c91ad00f0c66824176d26b2952
shubhamshri25 <105149595+shubhamshri25@users.noreply.github.com>
2023-02-11 18:55:59 +0530

    updated function

--- script.js
+++ script.js
@@ -8,20 +8,46 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map((student) => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((student) => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr.forEach((student, index) => {
+    if (student.marks < 50) {
+      arr.splice(index, 1);
+    }
+  });
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newStudents = [
+    { id: 4, name: "shubham", age: "21", marks: 88 },
+    { id: 5, name: "rahul", age: "20", marks: 70 },
+    { id: 6, name: "mohit", age: "22", marks: 75 }
+  ];
+  const combinedArray = arr.concat(newStudents);
+  console.log(combinedArray);
 }
